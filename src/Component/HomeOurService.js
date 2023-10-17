7d0043eee8e3f4d703e8ce6072768011231604c3
Sivasri234 <sivadhanyasri143@gmail.com>
2023-10-17 16:26:44 +0530

    recruit correction finished

--- src/Component/HomeOurService.js
+++ src/Component/HomeOurService.js
@@ -1,20 +1,48 @@
+import React,{useState,useEffect} from 'react';
 import Box from '../Assets/suitcaseBlack.png'
 import Announce from '../Assets/marketing.png'
 import Cart from '../Assets/trolley.png'
 import Tablet from '../Assets/tablet.jpg';
 import Mobile from '../Assets/smartphone.png';
+import ecommerce from '../Assets/corporate.jpg'
 import "./HomeOurService.css";
 
 
 function OurService() {
+    const digit = 'DIGITAL MARKETING';
+    const eCom = 'E-COMMERCE SOLUTIONS';
+    const web = 'WEB DEVELOPMENT';
+    const [showTabs, setShowTabs] = useState(digit)
+
+    
+    useEffect(() => {
+        const appearOptions = {
+            threshold: 0
+        };
+        const faders = document.querySelectorAll('.fade-in');
+        const appearOnScrool = new IntersectionObserver(function (entries, appearOnScrool) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    return;
+                }
+                else {
+                    entry.target.classList.add('appear');
+                    appearOnScrool.unobserve(entry.target);
+                }
+            })
+        }, appearOptions)
+        faders.forEach(fader => {
+            appearOnScrool.observe(fader);
+        })
+    });
     return (
         <>
-            <div class="container-fluid ourserviceWant"
+            <div class="container-fluid "
             >
 
-                <div class="row justify-content-center">
+                <div class="row ourserviceWant justify-content-center">
                     <div class="col-md-6 col-sm-6 col-xs-12 ps-5" style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                        <img src={Box} style={{ width: 50, height: 45 }} />
+                        <img className='imgour ' src={Box}   />
                         <h5 style={{ fontSize: 14, paddingTop: 20, marginLeft: 5 }}>WANT TO SEE MORE AMAZING WORKS?</h5>
                     </div>
                     <div class="col-md-6 col-sm-6 col-xs-12 ps-5">
@@ -35,23 +63,31 @@ function OurService() {
 
 
             <div class="container" style={{ marginTop: 50 }}>
-                <div class="row" style={{ paddingLeft: 70 }}>
-                    <div class="col-md-4 col-sm-4 col-xs-6">
-                        <img src={Announce} style={{ width: 55, height: 55, marginLeft: 55 }} />
-                        <p>DIGITAL MARKETING</p></div>
+                <div class=" iconsmark" style={{ paddingLeft: 70 }}>
+                    <div class="col-md-4 col-sm-4 col-xs-6" value="DIGITAL MARKETING" onClick={()=>{setShowTabs(digit)}} style={{ cursor: 'pointer' }}>
+                        <img className='annou' src={Announce}  />
+                        <p className='ppann'>DIGITAL MARKETING</p></div>
+<div className='vlccode1' >
 
-                    <div class="col-md-4 col-sm-4 col-xs-6">
-                        <img src={Cart} style={{ width: 55, height: 55, marginLeft: 55 }} />
-                        <p>E-COMMERCE SOLUTIONS</p></div>
+</div>
+                    <div class="col-md-4 col-sm-4 col-xs-6   carrrrrt" value="E-COMMERCE SOLUTIONS" onClick={()=>{setShowTabs(eCom)}} style={{ cursor: 'pointer' }} >
+                        <img src={Cart} className='cartt'  />
+                        <p className='ppann'>E-COMMERCE SOLUTIONS</p>
+                        </div>
+                        <div className='vlccode' >
 
-                    <div class="col-md-4 col-sm-4 col-xs-6">
-                        <img src={Mobile} style={{ width: 55, height: 55, marginLeft: 55 }} />
-                        <p>WEB DEVELOPMENT</p></div>
+</div>
+
+                    <div class="col-md-4 col-sm-4 col-xs-6 mobiii" value="WEB DEVELOPMENT" onClick={()=>{setShowTabs(web)}} style={{ cursor: 'pointer' }} >
+                        <img src={Mobile} className='mobbi'  />
+                        <p className='ppann'>WEB DEVELOPMENT</p></div>
                 </div>
             </div>
 
 
             <div class="container-fluid pt-5">
+            {
+                    showTabs == digit &&
                 <div class="ourService">
                     <div
                         class="col-md-6 col-sm-10 col-xs-10 Laptop1">
@@ -82,7 +118,77 @@ function OurService() {
 
                     </div>
                 </div>
+}
+{
+                        showTabs == eCom &&
+                            <div class="ourService">
+                                <div
+                                    class="col-md-6 col-sm-10 col-xs-10 Laptop1">
+                                    <img src={ecommerce} class="img-fluid Tablet"
+                                    />
+                                    <div
+                                        className='Digital'>
+                                        <div class="Laptop"
+                                            style={{ fontSize: 22 }}
+                                        >
+                                            <h1 style={{ marginLeft: 30, color: '#FDD947', fontSize: 30 }}>02</h1>
+                                            <p>E C O M M E R C E  S O L U T I O N S</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 col-sm-10 col-xs-10 d-flex  justify-content-center flex-column align-items-center Read1" >
+                                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                        <img src={Cart} style={{ width: 55, height: 55, marginLeft: 25, flex: 1 }} />
+                                        <h2 style={{ fontSize: 28, fontWeight: 700, flex: 1, marginTop: 30 }}>ECOMMERCE SOLUTIONS</h2>
+                                    </div>
+                                    <div style={{ padding: 30, backgroundColor: '#EFEFEF', marginTop: 30 }}>
+                                        <p style={{ fontSize: 14, lineHeight: 2, wordSpacing: 1, textAlign: 'center', width: '100%' }} class="col-md-6 col-sm-6 col-xs-10">We offer Ecommerce services for designing, developing, hosting, marketing and analytics, across industry verticals around the globe.</p>
+                                    </div>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                        <button class="Read">READ MORE <i class="bi bi-arrow-right"></i></button>
+                                    </div>
+
+                                </div>
+                            </div>
+}
+
+{
+                            showTabs == web ?
+                                <div class="ourService">
+                                    <div
+                                        class="col-md-6 col-sm-10 col-xs-10 Laptop1">
+                                        <img src={Tablet} class="img-fluid Tablet"
+                                        />
+                                        <div
+                                            className='Digital'>
+                                            <div class="Laptop"
+                                                style={{ fontSize: 22 }}
+                                            >
+                                                <h1 style={{ marginLeft: 30, color: '#FDD947', fontSize: 30 }}>03</h1>
+                                                <p>W E B  D E V E L O P M E N T</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6 col-sm-10 col-xs-10 d-flex  justify-content-center flex-column align-items-center Read1" >
+                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                            <img src={Mobile} style={{ width: 55, height: 55, marginLeft: 25, flex: 1 }} />
+                                            <h2 style={{ fontSize: 28, fontWeight: 700, flex: 1, marginTop: 30 }}>WEB DEVELOPMENT</h2>
+                                        </div>
+                                        <div style={{ padding: 30, backgroundColor: '#EFEFEF', marginTop: 30 }}>
+                                            <p style={{ fontSize: 14, lineHeight: 2, wordSpacing: 1, textAlign: 'center', width: '100%' }} class="col-md-6 col-sm-6 col-xs-10">We Make affordable & quality websites, which can offer you the best return for your investment. Create a successful online presence through good and extraordinary design according to your business.</p>
+                                        </div>
+                                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                            <button class="Read">READ MORE <i class="bi bi-arrow-right"></i></button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                : ''
+                }
             </div>
+
 
         </>
     )
--- src/Component/Recruit/Recruit.js
+++ src/Component/Recruit/Recruit.js
@@ -1,4 +1,74 @@
+// import React,{useEffect} from "react";
+// import "./Recruit.css"
+
+
+// function Recruit() {
+//     useEffect(() => {
+//         const appearOptions = {
+//           threshold : 0.5
+//         };
+//         const faders = document.querySelectorAll('.fade-in'); 
+//         const appearOnScro1l = new IntersectionObserver(function(entries,appearOnScrool){
+//           entries.forEach(entry =>{
+//             if(!entry.isIntersecting){
+//               return;
+//             }
+//             else{
+//               entry.target.classList.add('appear');
+//               appearOnScro1l.unobserve(entry.target);
+//             }
+//           })
+//         }, appearOptions)
+//         faders.forEach(fader =>{
+//           appearOnScro1l.observe(fader);
+//         })
+//       });
+//     return (
+//         <div className="iiiii animated animatedFadeInUp fadeInUp">
+                    
+//         <div className="carres   fade-in" >
+// <div style={{paddingLeft:50}}>
+// <h1 className="career1" >CAREERS</h1>
+
+// </div>
+// <div className="homm"  style={{color:"#575757",fontSize:8}}>
+//     HOME
+   
+// </div>
+// <div className="vl" style={{color:"#575757"}}>
+
+// </div>
+
+// <div className="care" style={{color:"#575757",fontSize:8}}>
+//     CAREERS
+// </div>
+                
+               
+
+//         </div>
+//         <div id="bg-image">
+    
+//            <div className="texxxt "  >
+//                <div style={{paddingLeft:50,paddingRight:50,paddingTop:50}}>
+//                <h1 class="white-text sm-small-text"style={{lineHeight:1,textAlign:"start",fontSize:"34px",fontFamily:"sans-serif",fontWeight:700}}>WE LOVE WHAT WE DO,<br/>
+// IT'S A WAY OF LIFE.</h1>
+//         <p class="text-large black-text margin-five" style={{fontSize:15 ,textAlign:"start"}}>Real challenge is scary, but we are not afraid.</p>
+//         <p class=" white-text" align="justify" style={{fontSize:13}} >If you are ready to make the difference, to face challenges and want to pursue a career in field of Java, J2EE, Hybris and Web Design &amp; Development, then right path is waiting for you.</p>
+//         <p className="we"  align="justify" style={{fontSize:13}}>We believe in giving right candidates to the suitable positions for an employer and our human resource to unleash their hidden potential which platform that each one of candidate deserves and needs to grow and acquire and every employee is an integral part of our organization and we take care for their development from corporate IT training to knowledge sharing.</p>
+// </div>
+// </div>
+//            </div>
+        
+
+
+
+//        </div>
+
+//     )
+// }
+// export default Recruit
 import React,{useEffect} from "react";
+
 import "./Recruit.css"
 
 
@@ -31,7 +101,7 @@ function Recruit() {
 <h1 className="career1" >CAREERS</h1>
 
 </div>
-<div className="homm"  style={{color:"#575757",fontSize:8}}>
+<div className="homm"  style={{color:"#575757",fontSize:11}}>
     HOME
    
 </div>
@@ -39,7 +109,7 @@ function Recruit() {
 
 </div>
 
-<div className="care" style={{color:"#575757",fontSize:8}}>
+<div className="care" style={{color:"#575757",fontSize:11}}>
     CAREERS
 </div>
                 
